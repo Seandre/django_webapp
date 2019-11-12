d8d21195040d1f60018d9304530f82eb45e12474
Seandre <seanandrepinzon@gmail.com>
2019-11-11 22:52:56 -0800

    Just added a template and learned a bit about views

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Blog</h1>')
+    return render(request, 'blog/home.html')
 
-
+def about(request):
+    return HttpResponse('<h2>About</h2>')
 
 
