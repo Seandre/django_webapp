eb8dee1be0dd822a713baad952fe43a266110adb
Seandre <seanandrepinzon@gmail.com>
2019-11-12 22:13:30 -0800

    Added some templates and CSS for style points.

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Sean',
+        'title': 'First Post',
+        'content': 'First content',
+        'date_posted': 'Nov 11, 2019'
+    },
+    {
+        'author': 'Hannah',
+        'title': 'Second Post',
+        'content': 'Second content',
+        'date_posted': 'Nov 12, 2019'
+    },
+
+]
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h2>About</h2>')
+    return render(request, 'blog/about.html', {'title' : 'About'})
 
 
